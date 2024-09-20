96f1e357db073c5c293af67efed8ba5825b58b8d
McRata0 <133047333+McRata0@users.noreply.github.com>
2024-09-20 10:54:00 -0300

    Add files via upload

---
+++ catequese/perguntas.js
@@ -0,0 +1,70 @@
+criaCartao(
+    'Conhecimentos gerais',
+    'qual capital da frança?',
+    'paris.'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    '?',
+    '21.'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Quais as posições do volei?',
+    'Oposto,Central,Ponteira,Libero,Levantador.'
+)
+
+criaCartao(
+    'Conhecimento gerais',
+    'Quem é o homem mais rico do mundo?',
+    'Jeff Bezos'
+)
+
+criaCartao(
+    'Conhecimento gerais',
+    'Quem é o homem mais rico do mundo?',
+    'Jeff Bezos'
+)
+
+criaCartao(
+    'Conhecimento gerais',
+    'Quando jesus morreu e nasceu?',
+    'morreuno ano 33 e nasceu no ano 0.'
+)
+
+criaCartao(
+    'Conhecimento gerais',
+    'Ate Quantos pontos o voleide areia vai?',
+    '21.'
+)
+
+criaCartao(
+    'Conhecimento gerais',
+    'Quais as posições do volei?',
+    'Oposto,Central,Ponteira,Libero,Levantador.'
+)
+
+criaCartao(
+    'Conhecimento gerais',
+    'Quem é o homem mais rico do mundo?',
+    'Jeff Bezos'
+)
+
+criaCartao(
+    'Conhecimento gerais',
+    'Quem é o homem mais rico do mundo?',
+    'Jeff Bezos'
+)
+criaCartao(
+    'Conhecimento gerais',
+    'Quando jesus morreu e nasceu?',
+    'morreuno ano 33 e nasceu no ano 0.'
+)
+
+criaCartao(
+    'Conhecimento gerais',
+    'Ate Quantos pontos o voleide areia vai?',
+    '21.'
+)
